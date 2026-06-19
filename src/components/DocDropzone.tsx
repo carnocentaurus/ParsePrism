5a600588554b066d384cda9d62888841a69134b9
EJay <carnocentaurus@gmail.com>
2026-06-19 16:34:08 +0800

    #15 - handleDrag function/handler

--- src/components/DocDropzone.tsx
+++ src/components/DocDropzone.tsx
@@ -7,3 +7,25 @@ interface DocDropzoneProps {
     onFileAccepted: (file: File) => void;
     status: ExtractionStatus;
 }
+
+
+export default function DocDropzone({onFileAccepted, status}: DocDropzoneProps): React.JSX.Element {
+    const [isDragActive, setIsDragActive] = useState<boolean>(false);
+
+    // 1. handle drag handlers
+    const handleDrag = (event: DragEvent<HTMLDivElement>) => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        if (event.type === 'dragenter' || event.type === 'dragover') {
+            setIsDragActive(true);
+        }
+        else if (event.type === 'dragleave') {
+            setIsDragActive(false);
+        }
+    }
+    
+    return(
+        <div></div>
+    );
+}
