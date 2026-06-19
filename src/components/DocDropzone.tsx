2de5de2aa239b2b3f17fb5ba56f58a8cdbf427ac
EJay <carnocentaurus@gmail.com>
2026-06-19 16:26:22 +0800

    #14 - DocDropzoneProps interface

--- src/components/DocDropzone.tsx
+++ src/components/DocDropzone.tsx
@@ -2,3 +2,8 @@
 
 import React, {useState, DragEvent, ChangeEvent} from 'react';
 import { ExtractionStatus } from '../hooks/useExtraction';
+
+interface DocDropzoneProps {
+    onFileAccepted: (file: File) => void;
+    status: ExtractionStatus;
+}
