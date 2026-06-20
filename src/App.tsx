d3ddcf79e321111905e37020acb9213c70eecb27
EJay <carnocentaurus@gmail.com>
2026-06-20 12:07:16 +0800

    #28 - App.tsx imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-export default function App() {
-    return(
-        <main>
-            <p className="font-righteous">PARSE PRISM</p>
-        </main>
-    );
-}
+// 06/18/2026
+
+// src/App.tsx
+
+import React from 'react';
+import { useExtraction } from './hooks/useExtraction';
+import DocDropzone from './components/DocDropzone';
+import DataPreviewTable from './components/DataPreviewTable';
